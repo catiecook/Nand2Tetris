af4f9e49d0027466eee300b613f669941db9e2e7
Tegan <tegan.straley@ucdenver.edu>
2016-04-26 10:43:23 -0600

    added classes

---
+++ Project10/XMLWriter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Project10
+{
+    class XMLWriter
+    {
+        public XMLWriter()
+        {
+
+        }
+
+        int tabLines = 0;
+
+        void WriteXML(string xmlString, StreamWriter outputFile)
+        {
+            for (int i = 0; i < tabLines; i++)
+            {
+                outputFile.Write("\t");
+            }
+            outputFile.WriteLine("<" + xmlString + ">");
+        }
+
+        void WriteXMLTag(Tokenizer.Token xmlTag, StreamWriter outputFile)
+        {
+            for (int i = 0; i < tabLines; i++)
+            {
+                outputFile.Write("\t");
+            }
+            outputFile.WriteLine("<" + xmlTag.type + ">" + xmlTag.context + "</" + xmlTag.type + ">");
+        }
+    }
+}
